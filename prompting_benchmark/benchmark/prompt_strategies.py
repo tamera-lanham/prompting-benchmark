ae51ec484471a6a11ecfe2a5f9af1cd6bd7d5e0b
Tamera Lanham <26015901+tamera-lanham@users.noreply.github.com>
2022-07-13 00:41:12 +0000

    refactor: trailing space allowed in prompt

--- prompting_benchmark/benchmark/prompt_strategies.py
+++ prompting_benchmark/benchmark/prompt_strategies.py
@@ -8,8 +8,8 @@ from typing import Callable
 def vanilla(question: str, scratchpad: list[str] = [], answer: str = "") -> str:
     if scratchpad:
         scratchpad_str = "\n".join(f"* {step}" for step in scratchpad)
-        return f"{question}\n{scratchpad_str}\n{answer}".strip()
-    return f"{question} {answer}".strip()
+        return f"{question}\n{scratchpad_str}\n{answer}"
+    return f"{question} {answer}"
 
 
 def q_and_a(question: str, scratchpad: list[str] = [], answer: str = "") -> str:
@@ -17,9 +17,9 @@ def q_and_a(question: str, scratchpad: list[str] = [], answer: str = "") -> str:
 
     if scratchpad:
         scratchpad_str = "\n".join(f"* {step}" for step in scratchpad)
-        return f"{q}\n{scratchpad_str}\n{a}".strip()
+        return f"{q}\n{scratchpad_str}\n{a}"
 
-    return f"{q}\n{a}".strip()
+    return f"{q}\n{a}"
 
 
 def from_template(question_template: str, answer_template: str, scratchpad_step_template: str = "* %s\n") -> Callable:
@@ -29,8 +29,8 @@ def from_template(question_template: str, answer_template: str, scratchpad_step_
 
         if scratchpad:
             scratchpad_str = "".join(scratchpad_step_template % step for step in scratchpad)
-            return (q_templated + scratchpad_str + a_templated).strip()
+            return q_templated + scratchpad_str + a_templated
 
-        return (q_templated + a_templated).strip()
+        return q_templated + a_templated
 
     return prompt_strategy
